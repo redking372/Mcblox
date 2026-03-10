c4592e5c2f458e0912b60b9483e8d9540029c754
redking372 <sboficial226@gmail.com>
2026-03-10 14:04:41 +0530

    Update Firebase SDK version and configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "YOUR_KEY_HERE",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyAqLisVJ9sgz56z_D0dFR1pOMaV6fFo5tw",
+    authDomain: "mcblox-bd512.firebaseapp.com",
+    projectId: "mcblox-bd512",
+    storageBucket: "mcblox-bd512.firebasestorage.app",
+    messagingSenderId: "150494713144",
+    appId: "1:150494713144:web:e498abc15700fdb1921dcd",
+    measurementId: "G-DXF8M78DRS"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
